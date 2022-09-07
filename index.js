d048a880981db81048065c421056888410cd17a0
Timon Jurschitsch <timon.jurschitsch@proton.me>
2022-09-07 21:16:18 +0200

    Change promise handling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as cheerio from 'cheerio';
 import download from 'download';
 import fetch from 'node-fetch';
 
-const getHTML = async () => {
+async function getMemes() {
   try {
     const response = await fetch(
       'https://memegen-link-examples-upleveled.netlify.app',
@@ -18,14 +18,17 @@ const getHTML = async () => {
     const firstTenMemes = imageURL.slice(0, 10);
     let counter = 1;
     firstTenMemes.forEach((url) => {
-      download(url, `${filePath}/0${counter}.jpg`).then(() =>
-        console.log('Download successful'),
-      );
+      download(url, `${filePath}/0${counter}.jpg`)
+        .then(() => console.log('Download successful'))
+        .catch((err) => console.log(err));
       counter++;
     });
+    return body;
   } catch (err) {
     console.log(err);
   }
-};
+}
 
-getHTML();
+getMemes()
+  .then(() => {})
+  .catch((err) => console.log(err));
